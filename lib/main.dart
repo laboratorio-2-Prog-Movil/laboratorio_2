2a0752dc60d2d389f3acf16d9f718e735ce82da8
Jose Andres <andres.henao9@udea.edu.co>
2025-04-26 21:06:57 -0500

    pagian que calcula el área del cuadrado lista, se hace a su vez una interfaz para la aplicación.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laboratorio_2/pages/SquareArea.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Geometría',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-     // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Squarearea(),
     );
   }
 }
